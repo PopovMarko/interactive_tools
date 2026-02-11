13e52e7423dc6c4a1fba0fff0dd5f3b81c639151
Mark Popov <hawk691@gmail.com>
2026-02-11 18:51:32 +0200

    Widget entitie and initialization add in widgets.go

--- pomo/app/widgets.go
+++ pomo/app/widgets.go
@@ -2,5 +2,139 @@ package app
 
 import (
 	"context"
+
 	"github.com/mum4k/termdash/cell"
+	"github.com/mum4k/termdash/widgets/donut"
+	"github.com/mum4k/termdash/widgets/segmentdisplay"
+	"github.com/mum4k/termdash/widgets/text"
 )
+
+type widgets struct {
+	donTimer       *donut.Donut
+	disType        *segmentdisplay.SegmentDisplay
+	txtInfo        *text.Text
+	txtTimer       *text.Text
+	updateDonTimer chan []int
+	updateDisType  chan string
+	updateTxtInfo  chan string
+	updateTxtType  chan string
+}
+
+func (w *widgets) update(timer []int, txtDisType, txtInfo, txtTxtType string, redrawCh chan<- bool) {
+	if len(timer) != 0 {
+		w.updateDonTimer <- timer
+	}
+
+	if txtDisType != "" {
+		w.updateDisType <- txtDisType
+	}
+
+	if txtInfo != "" {
+		w.updateTxtInfo <- txtInfo
+	}
+
+	if txtTxtType != "" {
+		w.updateTxtType <- txtTxtType
+	}
+
+	redrawCh <- true
+}
+
+func newWidgets(ctx context.Context, errorCh chan<- error) (*widgets, error) {
+	w := &widgets{}
+	var err error
+
+	w.updateDonTimer = make(chan []int)
+	w.updateDisType = make(chan string)
+	w.updateTxtInfo = make(chan string)
+	w.updateTxtType = make(chan string)
+
+	w.donTimer, err = newDonTimer(ctx, w.updateDonTimer, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	w.disType = newDisType(ctx, w.updateDisType, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	w.txtInfo = newTxtInfo(ctx, w.updateTxtInfo, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	w.txtTimer = newTxtTimer(ctx, w.updateTxtType, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	return w, nil
+}
+
+func newDonTimer(ctx context.Context, donUpdater <-chan []int, errorCh chan<- error) (*donut.Donut, error) {
+	don, err := donut.New(donut.Clockwise(), donut.CellOpts(cell.FgColor(cell.ColorBlue)))
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			select {
+			case d := <-donUpdater:
+				if d[0] <= d[1] {
+					return
+				}
+				errorCh <- don.Absolute(d[0], d[1])
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return don, nil
+}
+
+func newDisType(ctx context.Context, disUpdater <-chan string, errorCh chan<- error) (*segmentdisplay.SegmentDisplay, error) {
+	dis, err := segmentdisplay.New()
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			select {
+			case d, ok := <-disUpdater:
+				if !ok {
+					return
+				}
+				if d == "" {
+					d = " "
+				}
+				errorCh <- dis.Write([]*segmentdisplay.TextChunk{segmentdisplay.NewChunk(d)})
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return dis, nil
+}
+
+func newText(ctx context.Context, textUpdater <-chan string, errorCh chan<- error) (*text.Text, error) {
+	txt, err := text.New()
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			select {
+			case t, ok := <-textUpdater:
+				if !ok {
+					return
+				}
+				errorCh <- txt.Write(t)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return txt, nil
+}
--- pomo/go.mod
+++ pomo/go.mod
@@ -3,3 +3,8 @@ module pragprog.com/rggo/interactive_tools/pomo
 go 1.25.6
 
 require github.com/mum4k/termdash v0.20.0
+
+require (
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+)
